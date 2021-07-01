449ecaefe9a7cc40ea4c6531bbc6688898e2a1c4
sararighetti <sararighe.sr@gmail.com>
2021-07-01 15:43:05 +0200

    added products img, created product card in html, created data in js

---
+++ script.js
@@ -0,0 +1,24 @@
+data = [
+    {
+        id:1,
+        name: "JUG",
+        img: 'src="img/jugs.jpg" alt="jug"',
+        price: 24.99,
+        description: "Pottery jugs"
+    },
+    {
+        id:2,
+        name: "BOWL",
+        img: 'src="img/bowls.jpg" alt="bowls"',
+        price: 30.99,
+        description: "Pottery Bowls"
+    }
+    ,
+    {
+        id:3,
+        name: "BOWL",
+        img: 'src="img/bowls.jpg" alt="bowls"',
+        price: 30.99,
+        description: "Pottery Bowls"
+    }
+]
